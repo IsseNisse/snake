92dc483434563fb48753d7db352c0d5df0e34612
IsseNisse <isak.vindeln@gmail.com>
2020-11-30 13:28:43 +0100

    startade på collision detection

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 500
 
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 player_pos = [400, 300]
+fruit_pos = [random.randint(0, WIDTH), random.randint(0, HEIGHT)]
 
-new_x = 5
+new_x = 10
 new_y = 0
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -18,6 +21,13 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 game_over = False
+
+def detect_collision(player_pos, fruit_pos):
+    player_x = player_pos[0]
+    player_y = player_pos[1]
+
+    fruit_x = fruit_pos[0]
+    fruit_y = fruit_pos[1]
 
 while not game_over:
     for event in pygame.event.get():
@@ -28,41 +38,42 @@ while not game_over:
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_LEFT:
-                if new_x == 5:
-                    new_x = 5
+                if new_x == 10:
+                    new_x = 10
                     new_y = 0
                 else:
-                    new_x = -5
+                    new_x = -10
                     new_y = 0
 
             elif event.key == pygame.K_RIGHT:
-                if new_x == -5:
-                    new_x = -5
+                if new_x == -10:
+                    new_x = -10
                     new_y = 0
                 else:
-                    new_x = 5
+                    new_x = 10
                     new_y = 0
             elif event.key == pygame.K_UP:
-                if new_y == 5:
+                if new_y == 10:
                     new_x = 0
-                    new_y = 5
+                    new_y = 10
                 else:
                     new_x = 0
-                    new_y = -5
+                    new_y = -10
             elif event.key == pygame.K_DOWN:
-                if new_y == -5:
+                if new_y == -10:
                     new_x = 0
-                    new_y = -5
+                    new_y = -10
                 else:
                     new_x = 0
-                    new_y = 5
+                    new_y = 10
             player_pos = [player_pos[0] + new_x, player_pos[1] + new_y]
 
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, WHITE, (player_pos[0], player_pos[1], 1, 1))
+    pygame.draw.rect(screen, WHITE, (player_pos[0], player_pos[1], 10, 10))
+    pygame.draw.rect(screen, RED, (fruit_pos[0], fruit_pos[1], 10, 10))
     player_pos = [player_pos[0] + new_x, player_pos[1] + new_y]
 
-    clock.tick(60)
+    clock.tick(10)
 
     pygame.display.update()
